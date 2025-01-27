108e1b852210cf443ccac9cd766ca28b679f84df
KristofferHoff <kristoffer.hoff@me.com>
2025-01-27 12:29:45 +0100

    oppdatering

--- LEGOdudes_REACT/src/App.jsx
+++ LEGOdudes_REACT/src/App.jsx
@@ -18,19 +18,7 @@ function App() {
       <Cart />
 
       {/* Overskrift-seksjon med logo og handlekurvknapp */}
-      <header>
-        <a href="index.html" id="logo">
-          <img src="website_images/LD_logo.svg" alt="Legodudes" />
-        </a>
-        <button id="shoppingcart">
-          <span id="cartcount">99</span>{" "}
-          {/* Viser antall varer i handlekurven */}
-          <img src="website_images/legocart.svg" alt="Handlevogn" />
-        </button>
-
-        {/* Navigasjonskomponenten */}
-        <Nav />
-      </header>
+      
 
       {/* Hovedinnhold */}
       <main>
---
+++ LEGOdudes_REACT/src/components/header.jsx
@@ -0,0 +1,17 @@
+export default function header(){
+    return(
+        <header>
+        <a href="index.html" id="logo">
+          <img src="website_images/LD_logo.svg" alt="Legodudes" />
+        </a>
+        <button id="shoppingcart">
+          <span id="cartcount">99</span>{" "}
+          {/* Viser antall varer i handlekurven */}
+          <img src="website_images/legocart.svg" alt="Handlevogn" />
+        </button>
+
+        {/* Navigasjonskomponenten */}
+        <Nav />
+      </header>
+    );
+}
